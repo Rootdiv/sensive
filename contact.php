56f3518aa46aea4ac928152fc7d160cc3e7268ae
Rootdiv <rootdiv@mail.ru>
2022-01-21 01:58:46 +0300

    feat: contact page

--- contact.php
+++ contact.php
@@ -9,11 +9,11 @@ get_header();?>
       <div class="container">
         <div class="hero-banner hero-banner--sm">
           <div class="hero-banner__content">
-            <h1>Contact Us</h1>
+            <h1>Связаться с нами</h1>
             <nav aria-label="breadcrumb" class="banner-breadcrumb">
               <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
+                <li class="breadcrumb-item"><a href="<?=get_home_url();?>">Главная</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Связаться с нами</li>
               </ol>
             </nav>
           </div>
@@ -26,28 +26,7 @@ get_header();?>
     <section class="section-margin--small section-margin">
       <div class="container">
         <div class="d-none d-sm-block mb-5 pb-4">
-          <div id="map" style="height: 420px"></div>
-          <script>
-            function initMap() {
-              var uluru = { lat: -25.363, lng: 131.044 };
-              var grayStyles = [{
-                  featureType: 'all',
-                  stylers: [{ saturation: -90 }, { lightness: 50 }],
-                },
-                { elementType: 'labels.text.fill', stylers: [{ color: '#A3A3A3' }] },
-              ];
-              var map = new google.maps.Map(document.getElementById('map'), {
-                center: { lat: -31.197, lng: 150.744 },
-                zoom: 9,
-                styles: grayStyles,
-                scrollwheel: false,
-              });
-            }
-
-          </script>
-          <script
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpfS1oRGreGSBU5HHjMmQ3o5NLw7VdJ6I&callback=initMap">
-          </script>
+          <?php the_content();?>
         </div>
 
         <div class="row">
@@ -55,52 +34,27 @@ get_header();?>
             <div class="media contact-info">
               <span class="contact-info__icon"><i class="ti-home"></i></span>
               <div class="media-body">
-                <h3>California United States</h3>
-                <p>Santa monica bullevard</p>
+                <h3><?php the_field('address_city');?></h3>
+                <p><?php the_field('address');?></p>
               </div>
             </div>
             <div class="media contact-info">
               <span class="contact-info__icon"><i class="ti-headphone"></i></span>
               <div class="media-body">
-                <h3><a href="tel:454545654">00 (440) 9865 562</a></h3>
-                <p>Mon to Fri 9am to 6pm</p>
+                <h3><a href="tel:<?php the_field('phone');?>"><?php the_field('phone');?></a></h3>
+                <p><?php the_field('time_works');?></p>
               </div>
             </div>
             <div class="media contact-info">
               <span class="contact-info__icon"><i class="ti-email"></i></span>
               <div class="media-body">
-                <h3><a href="mailto:support@colorlib.com">support@colorlib.com</a></h3>
-                <p>Send us your query anytime!</p>
+                <h3><a href="mailto:<?php the_field('email');?>"><?php the_field('email');?></a></h3>
+                <p><?php the_field('email_text');?></p>
               </div>
             </div>
           </div>
-          <div class="col-md-8 col-lg-9">
-            <form action="#/" class="form-contact contact_form" action="contact_process.php" method="post"
-              id="contactForm" novalidate="novalidate">
-              <div class="row">
-                <div class="col-lg-5">
-                  <div class="form-group">
-                    <input class="form-control" name="name" id="name" type="text" placeholder="Enter your name" />
-                  </div>
-                  <div class="form-group">
-                    <input class="form-control" name="email" id="email" type="email"
-                      placeholder="Enter email address" />
-                  </div>
-                  <div class="form-group">
-                    <input class="form-control" name="subject" id="subject" type="text" placeholder="Enter Subject" />
-                  </div>
-                </div>
-                <div class="col-lg-7">
-                  <div class="form-group">
-                    <textarea class="form-control different-control w-100" name="message" id="message" cols="30"
-                      rows="5" placeholder="Enter Message"></textarea>
-                  </div>
-                </div>
-              </div>
-              <div class="form-group text-center text-md-right mt-3">
-                <button type="submit" class="button button--active button-contactForm">Send Message</button>
-              </div>
-            </form>
+          <div class="col-md-8 col-lg-9 form-contact contact_form">
+            <?=do_shortcode('[contact-form-7 id="110" title="Контактная форма"]');?>
           </div>
         </div>
       </div>
